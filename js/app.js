3db10cf4eb543b96df7e19242386f1497825dfa7
pushnoy100500 <brainstation@BrainStations-MacBook-Air-2.local>
2015-07-06 16:28:43 -0400

    more stuff

--- js/app.js
+++ js/app.js
@@ -3,7 +3,22 @@ app.config(function($routeProvider) {
 	$routeProvider.
 		when('/home', {
 			templateUrl: 'htmls/search.html',
-			controller: 'Search as ctrl'
+			controller: 'MySearch as ctrl',
+			resolve:{
+				app: function($q, $http) {
+					var defer = $q.defer();
+					$http.get('http://www.gifbase.com/js/tags.json').
+				  	success(function(data, status, headers, config) {
+				  	console.log(data);
+				    defer.resolve(data);
+ 					 }).
+					error(function(data, status, headers, config) {
+					   console.log(data);
+					   alert("there was a problem pulling tags out");
+					});
+					return defer.promise;
+				}
+			}
 		})
 		.when('/gifDetails/:gifId', {
 			templateUrl: '',
--- js/searchCtrl.js
+++ js/searchCtrl.js
@@ -1,4 +1,21 @@
-function Search() {
-
+function MySearch(app) {
+	this.allTags = app;
+	this.isTagExist = false;
 }
-angular.module('MyApp').controller("Search", Search);
+MySearch.prototype.logAllTags = function(){
+	console.log(this.allTags);
+};
+MySearch.prototype.checkTagExist = function(tag){	
+	for(var i = 0; i < this.allTags.length; i++) {
+		if(tag === this.allTags[i]) {
+			this.isTagExist = true;
+			console.log(this.allTags[i]);
+			return "exist";
+			break;
+		}
+	}
+	this.isTagExist = false;
+	console.log('tag not found');
+
+};
+angular.module('MyApp').controller("MySearch", MySearch);
